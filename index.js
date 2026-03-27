36165ec7f28448edfab3fe56f11e03e6c48eb822
ellis-84 <eric@shuckabuck.com>
2026-03-27 17:06:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.post('/api/generate-video', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
